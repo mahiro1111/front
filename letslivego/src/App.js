81da5008b045ed4274c8a6c57b468c3a8861bdfd
mahiro1111 <148053248+mahiro1111@users.noreply.github.com>
2024-06-02 16:01:24 +0900

    test commit

--- letslivego/src/App.js
+++ letslivego/src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import { UIProvider } from "@yamada-ui/react"
 import Demo from './maincomponent';
+import Hoge from './box';
 
 
 function App() {
   return (
+    <div class="main">
     <UIProvider>
       <Demo />
+      <Hoge />
     </UIProvider>
+    <p>
+      Hello World!
+    </p>
+    </div>
+    
   );
 }
 
---
+++ letslivego/src/box.js
@@ -0,0 +1,11 @@
+import { Box } from "@yamada-ui/react"
+
+const Hoge = () => {
+  return( 
+  <Box as="button" p="md" rounded="md" bg="rgb(183, 168, 255)" color="blue">
+  This is the box
+  </Box>
+  )
+}
+
+export default Hoge;

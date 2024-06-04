18ff9f6d15b857d1820622635123b34fc2820062
mitok1212 <mitokatayama1231003@gmail.com>
2024-06-04 16:40:22 +0900

    合わせました

--- letslivego/src/Ainote.js
+++ letslivego/src/Ainote.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+//import { Button } from 'react-bootstrap';
 
 const Ainote = () => {
   return (
-    <div>
-        kasi hyouzi 
+    <div className="Ainotecontainer">
+      <div className="Ainotesidebar">
+        <h2>Genre</h2>
+        <div className="Ainotesearch">
+          <label htmlFor="Ainotesearch">Music Search</label>
+          <input type="text" id="search" placeholder="Search" />
+        </div>
+        <div className="Ainotelist">
+          <h3>Music List</h3>
+          <ul>
+            <li>---</li>
+            <li>---</li>
+            <li>---</li>
+            <li>---</li>
+            <li>---</li>
+          </ul>
+        </div>
+      </div>
+      <div className="Ainotecontent">
+        <p>歌詞表示される</p>
+      </div>
     </div>
   )
 }
